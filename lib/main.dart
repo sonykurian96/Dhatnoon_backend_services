5e4f6b61c81c4c0a4afb7347dad5c5b081e09dea
sonykurian96 <sonykurian96@gmail.com>
2022-07-04 09:13:25 +0530

    added agora video call

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final AgoraClient client = AgoraClient(
+    agoraConnectionData: AgoraConnectionData(
+      appId: "63a29f76b5704dd0bf01316fc9f8f736",
+      channelName: "test",
+      // username: "user",
+    ),
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    initAgora();
+  }
+
+  void initAgora() async {
+    await client.initialize();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Agora UIKit'),
+          centerTitle: true,
+        ),
+        body: SafeArea(
+          child: Stack(
+            children: [
+              AgoraVideoViewer(
+                client: client,
+                layoutType: Layout.floating,
+                enableHostControls: true, // Add this to enable host controls
+              ),
+              AgoraVideoButtons(
+                client: client,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
