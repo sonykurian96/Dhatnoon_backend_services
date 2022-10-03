e18e4c84c51e0eddd2531dd13b8ccdaa6c96db73
Sony Kurian <67534990+sonykurian96@users.noreply.github.com>
2022-10-03 19:53:43 +0530

    Update intermediate.dart

--- lib/intermediate.dart
+++ lib/intermediate.dart
@@ -5,7 +5,7 @@ class Generator{
 
   static Future<String> getCode() async {
     String link =
-        "https://agora-node-tokenserver-1.davidcaleb.repl.co/access_token?channelName=test";
+        "serverlink";
 
     Response response = await get(Uri.parse(link));
     Map data = jsonDecode(response.body);
